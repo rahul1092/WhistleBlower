59fc462484a5b6530f77c6d15e86bdbce3f36c2c
Rahul Sharma <rahulsharma1092@gmail.com>
2020-01-04 13:24:07 +0530

    Added uwsgi configuration

--- settings/settings_dev.py
+++ settings/settings_dev.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^w98vtja^k#wzfub=zbtkymawrolx-c%n1ag#s1h4w8j*kqoxn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rahuls.xyz']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
